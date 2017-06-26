afd7a84fe1c999f529f95d38ae433ff6fcf6751f
박효완 <qkrgy1206@naver.com>
2017-06-27 03:00:05 +0900

    색보기, 커서 잡기

--- Drawing Board/Drawing Board/BoardControl.xaml.cs
+++ Drawing Board/Drawing Board/BoardControl.xaml.cs
@@ -27,15 +27,34 @@ namespace Drawing_Board
         bool isPainting = false;
         bool isErasing = false;
         bool isSpoiding = false;
+        bool isCursorClick = false;
         Point currentPoint = new Point();
         //Point currentPoint2 = new Point();
         Line line;
         Brush brush = Brushes.Black;
+        bool captured = false;
+        double x_shape, x_canvas, y_shape, y_canvas;
+
+        // -----------------------------
+        AdornerLayer adornerLayer;
+
+        bool _isDown;
+        bool _isDragging;
+        bool selected = false;
+        UIElement selectedElement = null;
+        Point _startPoint;
+        private double _originalLeft;
+        private double _originalTop;
+
         public BoardControl()
         {
             InitializeComponent();
             btn_eraser.Click += btn_eraser_Click;
             btn_circle.Click += btn_circle_Click;
+            //this.MouseLeftButtonDown += new MouseButtonEventHandler(Window_MouseLeftButtonDown);
+            //this.MouseLeftButtonUp += new MouseButtonEventHandler(DragFinishedMouseHandler);
+            this.MouseMove += new MouseEventHandler(Window_MouseMove);
+            this.MouseLeave += new MouseEventHandler(Window_MouseLeave);
         }
         
         private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
@@ -43,6 +62,38 @@ namespace Drawing_Board
             if (e.ButtonState == MouseButtonState.Pressed)
             {
                 currentPoint = e.GetPosition(this);
+                if(isCursorClick == true)
+                {
+                    if (selected)
+                    {
+                        selected = false;
+                        if (selectedElement != null)
+                        {
+                            // Remove the adorner from the selected element
+                            paintSurface.ReleaseMouseCapture();
+                            adornerLayer.Remove(adornerLayer.GetAdorners(selectedElement)[0]);
+                            selectedElement = null;
+                        }
+                    }
+
+                    // If any element except canvas is clicked, 
+                    // assign the selected element and add the adorner
+                    if (e.Source != paintSurface)
+                    {
+                        _isDown = true;
+                        _startPoint = e.GetPosition(paintSurface);
+
+                        selectedElement = e.Source as UIElement;
+
+                        _originalLeft = Canvas.GetLeft(selectedElement);
+                        _originalTop = Canvas.GetTop(selectedElement);
+
+                        adornerLayer = AdornerLayer.GetAdornerLayer(selectedElement);
+                        adornerLayer.Add(new ResizeShapes(selectedElement));
+                        selected = true;
+                        e.Handled = true;
+                    }
+                }
             }
         }
         
@@ -51,7 +102,6 @@ namespace Drawing_Board
             if (e.LeftButton == MouseButtonState.Pressed && (isDrawing==true || isErasing == true))
             {
                 line = drawStraight(e.GetPosition(this).X, e.GetPosition(this).Y );
-
                 currentPoint = e.GetPosition(this);
                 paintSurface.Children.Add(line);
             }
@@ -59,6 +109,7 @@ namespace Drawing_Board
 
         private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
         {
+            
             if (e.ButtonState == MouseButtonState.Released)
             {
                 //currentPoint2 = e.GetPosition(this);
@@ -76,76 +127,14 @@ namespace Drawing_Board
                     line = drawStraight(e.GetPosition(this).X, e.GetPosition(this).Y);
                     paintSurface.Children.Add(line);
                 }
+                else if(isCursorClick == true)
+                {
+                    DragFinishedMouseHandler(sender, e);
+                    paintSurface.ReleaseMouseCapture();
+                }
             }
         }
         
-
-        private void btn_eraser_Click(object sender, RoutedEventArgs e)
-        {
-            isDrawing = false;
-            isCircleDrawing = false;
-            isRectangleDrawing = false;
-            isLineDrawing = false;
-            isPainting = false;
-            isErasing = true;
-            isSpoiding = false;
-        }
-
-        private void btn_circle_Click(object sender, RoutedEventArgs e)
-        {
-            isDrawing = false;
-            isCircleDrawing = true;
-            isRectangleDrawing = false;
-            isLineDrawing = false;
-            isPainting = false;
-            isErasing = false;
-            isSpoiding = false;
-        }
-
-        private void btn_pen_Click(object sender, RoutedEventArgs e)
-        {
-            isDrawing = true;
-            isCircleDrawing = false;
-            isRectangleDrawing = false;
-            isLineDrawing = false;
-            isPainting = false;
-            isErasing = false;
-            isSpoiding = false;
-        }
-
-        private void btn_rectangle_Click(object sender, RoutedEventArgs e)
-        {
-            isDrawing = false;
-            isCircleDrawing = false;
-            isRectangleDrawing = true;
-            isLineDrawing = false;
-            isPainting = false;
-            isErasing = false;
-            isSpoiding = false;
-        }
-
-        private void btn_line_Click(object sender, RoutedEventArgs e)
-        {
-            isDrawing = false;
-            isCircleDrawing = false;
-            isRectangleDrawing = false;
-            isLineDrawing = true;
-            isPainting = false;
-            isErasing = false;
-            isSpoiding = false;
-        }
-
-        private void btn_paint_Click(object sender, RoutedEventArgs e)
-        {
-            isPainting = true;
-            isDrawing = false;
-            isCircleDrawing = false;
-            isRectangleDrawing = false;
-            isLineDrawing = false;
-            isErasing = false;
-            isSpoiding = false;
-        }
-
         private void drawEllipse(double width, double heigth)
         {
             Ellipse ellipse = new Ellipse();
@@ -155,7 +144,7 @@ namespace Drawing_Board
 
             ellipse.Fill = Brushes.Transparent;
 
-            ellipse.Height = Math.Abs((heigth - 77) - (currentPoint.Y - 77));
+            ellipse.Height = Math.Abs(heigth - currentPoint.Y);
             ellipse.Width = Math.Abs(width - currentPoint.X);
 
             if (width - currentPoint.X < 0)
@@ -169,7 +158,7 @@ namespace Drawing_Board
                 Canvas.SetRight(ellipse, width);
             }
 
-            if ((heigth - 77) - (currentPoint.Y - 77) < 0)
+            if (heigth - currentPoint.Y  < 0)
             {
                 Canvas.SetTop(ellipse, heigth - 77);
                 Canvas.SetBottom(ellipse, currentPoint.Y - 77);
@@ -181,6 +170,9 @@ namespace Drawing_Board
             }
 
             ellipse.MouseDown += new MouseButtonEventHandler(Ellipse_OnMouseDown);
+            //ellipse.MouseMove += new MouseEventHandler(shape_MouseMove);
+            //ellipse.MouseLeftButtonUp += new MouseButtonEventHandler(shape_MouseLeftButtonUp);
+
             paintSurface.Children.Add(ellipse);
         }
 
@@ -192,7 +184,7 @@ namespace Drawing_Board
 
             rectangle.Fill = Brushes.Transparent;
 
-            rectangle.Height = Math.Abs((heigth - 77) - (currentPoint.Y - 77));
+            rectangle.Height = Math.Abs(heigth  - currentPoint.Y);
             rectangle.Width = Math.Abs(width - currentPoint.X);
 
             if (width - currentPoint.X < 0)
@@ -206,7 +198,7 @@ namespace Drawing_Board
                 Canvas.SetRight(rectangle, width);
             }
 
-            if ((heigth - 77) - (currentPoint.Y - 77) < 0)
+            if (heigth- currentPoint.Y  < 0)
             {
                 Canvas.SetTop(rectangle, heigth - 77);
                 Canvas.SetBottom(rectangle, currentPoint.Y - 77);
@@ -218,6 +210,9 @@ namespace Drawing_Board
             }
 
             rectangle.MouseDown += new MouseButtonEventHandler(Rectangle_OnMouseDown);
+            //rectangle.MouseMove += new MouseEventHandler(shape_MouseMove);
+            //rectangle.MouseLeftButtonUp += new MouseButtonEventHandler(shape_MouseLeftButtonUp);
+
             paintSurface.Children.Add(rectangle);
         }
 
@@ -238,6 +233,11 @@ namespace Drawing_Board
                 else if (isSpoiding == true)
                 {
                     brush = circle.Fill;
+                    spoideRect.Fill = circle.Fill;
+                }
+                else if(isCursorClick == true)
+                {
+                    shape_MouseDown(circle, e);
                 }
             }
         }
@@ -259,9 +259,47 @@ namespace Drawing_Board
                 else if (isSpoiding == true)
                 {
                     brush = rect.Fill;
+                    spoideRect.Fill = rect.Fill;
+                }
+                else if(isCursorClick == true)
+                {
+                    shape_MouseDown(rect, e);
                 }
             }
+        }
 
+        private void shape_MouseDown(UIElement shape, MouseButtonEventArgs e)
+        {
+            Mouse.Capture(shape);
+            captured = true;
+            x_shape = Canvas.GetLeft(shape);
+            x_canvas = e.GetPosition(paintSurface).X;
+            y_shape = Canvas.GetTop(shape);
+            y_canvas = e.GetPosition(paintSurface).Y;
+            ReleaseMouseCapture();
+        }
+
+        private void shape_MouseMove(object sender, MouseEventArgs e)
+        {
+            UIElement source = sender as UIElement;
+            if (captured)
+            {
+                double x = e.GetPosition(paintSurface).X;
+                double y = e.GetPosition(paintSurface).Y;
+                x_shape += x - x_canvas;
+                Canvas.SetLeft(source, x_shape);
+                x_canvas = x;
+                y_shape += y - y_canvas;
+                Canvas.SetTop(source, y_shape);
+                y_canvas = y;
+            }
+        }
+
+        private void shape_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Mouse.Capture(null);
+            ReleaseMouseCapture();
+            captured = false;
         }
 
         private Line drawStraight(double width, double heigth)
@@ -312,8 +350,87 @@ namespace Drawing_Board
             brush = Brushes.Purple;
         }
 
+        private void btn_eraser_Click(object sender, RoutedEventArgs e)
+        {
+            paintSurface.Cursor = Cursors.Arrow;
+            isDrawing = false;
+            isCircleDrawing = false;
+            isRectangleDrawing = false;
+            isLineDrawing = false;
+            isPainting = false;
+            isErasing = true;
+            isSpoiding = false;
+            isCursorClick = false;
+        }
+
+        private void btn_circle_Click(object sender, RoutedEventArgs e)
+        {
+            paintSurface.Cursor = Cursors.Cross;
+            isDrawing = false;
+            isCircleDrawing = true;
+            isRectangleDrawing = false;
+            isLineDrawing = false;
+            isPainting = false;
+            isErasing = false;
+            isSpoiding = false;
+            isCursorClick = false;
+        }
+
+        private void btn_pen_Click(object sender, RoutedEventArgs e)
+        {
+            paintSurface.Cursor = Cursors.Pen;
+            isDrawing = true;
+            isCircleDrawing = false;
+            isRectangleDrawing = false;
+            isLineDrawing = false;
+            isPainting = false;
+            isErasing = false;
+            isSpoiding = false;
+            isCursorClick = false;
+        }
+
+        private void btn_rectangle_Click(object sender, RoutedEventArgs e)
+        {
+            paintSurface.Cursor = Cursors.Cross;
+            isDrawing = false;
+            isCircleDrawing = false;
+            isRectangleDrawing = true;
+            isLineDrawing = false;
+            isPainting = false;
+            isErasing = false;
+            isSpoiding = false;
+            isCursorClick = false;
+        }
+
+        private void btn_line_Click(object sender, RoutedEventArgs e)
+        {
+            paintSurface.Cursor = Cursors.Cross;
+            isDrawing = false;
+            isCircleDrawing = false;
+            isRectangleDrawing = false;
+            isLineDrawing = true;
+            isPainting = false;
+            isErasing = false;
+            isSpoiding = false;
+            isCursorClick = false;
+        }
+
+        private void btn_paint_Click(object sender, RoutedEventArgs e)
+        {
+            paintSurface.Cursor = Cursors.Arrow;
+            isPainting = true;
+            isDrawing = false;
+            isCircleDrawing = false;
+            isRectangleDrawing = false;
+            isLineDrawing = false;
+            isErasing = false;
+            isSpoiding = false;
+            isCursorClick = false;
+        }
+
         private void btn_spoide_Click(object sender, RoutedEventArgs e)
         {
+            paintSurface.Cursor = Cursors.Arrow;
             isPainting = false;
             isDrawing = false;
             isCircleDrawing = false;
@@ -321,11 +438,75 @@ namespace Drawing_Board
             isLineDrawing = false;
             isErasing = false;
             isSpoiding = true;
+            isCursorClick = false;
         }
 
         private void btn_cursor_Click(object sender, RoutedEventArgs e)
         {
+            paintSurface.Cursor = Cursors.Arrow;
+            isCursorClick = true;
+            isRectangleDrawing = false;
+            isPainting = false;
+            isDrawing = false;
+            isCircleDrawing = false;
+            isRectangleDrawing = false;
+            isLineDrawing = false;
+            isErasing = false;
+            isSpoiding = false;
+        }
+        void Window_MouseLeave(object sender, MouseEventArgs e)
+        {
+            StopDragging();
+            e.Handled = true;
+        }
 
+        // Handler for drag stopping on user choise
+        void DragFinishedMouseHandler(object sender, MouseButtonEventArgs e)
+        {
+            StopDragging();
+            e.Handled = true;
+            paintSurface.ReleaseMouseCapture();
+        }
+
+        // Method for stopping dragging
+        private void StopDragging()
+        {
+            if (_isDown)
+            {
+                _isDown = false;
+                _isDragging = false;
+            }
+        }
+
+        // Hanler for providing drag operation with selected element
+        void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isDown)
+            {
+                if ((_isDragging == false) &&
+                    ((Math.Abs(e.GetPosition(paintSurface).X - _startPoint.X) > SystemParameters.MinimumHorizontalDragDistance) ||
+                    (Math.Abs(e.GetPosition(paintSurface).Y - _startPoint.Y) > SystemParameters.MinimumVerticalDragDistance)))
+                    _isDragging = true;
+
+                if (_isDragging)
+                {
+                    Point position = Mouse.GetPosition(paintSurface);
+                    Canvas.SetTop(selectedElement, position.Y - (_startPoint.Y - _originalTop));
+                    Canvas.SetLeft(selectedElement, position.X - (_startPoint.X - _originalLeft));
+                }
+            }
+        }
+        void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (selected)
+            {
+                selected = false;
+                if (selectedElement != null)
+                {
+                    adornerLayer.Remove(adornerLayer.GetAdorners(selectedElement)[0]);
+                    selectedElement = null;
+                }
+            }
         }
     }
 }

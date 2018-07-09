4d95b5361c646235289ecac7f881c878a2a29227
박효완 <qkrgy1206@naver.com>
2018-07-09 22:06:12 +0900

    쓸데없는 주석 지우기

--- Drawing Board/Drawing Board/ResizeShapes.cs
+++ Drawing Board/Drawing Board/ResizeShapes.cs
@@ -26,22 +26,18 @@ namespace Drawing_Board
             : base(adornedElement)
         {
             visualChildren = new VisualCollection(this);
-
-            // Call a helper method to initialize the Thumbs
-            // with a customized cursors.
+            
             BuildAdornerCorner(ref topLeft, Cursors.SizeNWSE);
             BuildAdornerCorner(ref topRight, Cursors.SizeNESW);
             BuildAdornerCorner(ref bottomLeft, Cursors.SizeNESW);
             BuildAdornerCorner(ref bottomRight, Cursors.SizeNWSE);
-
-            // Add handlers for resizing.
+            
             bottomLeft.DragDelta += new DragDeltaEventHandler(HandleBottomLeft);
             bottomRight.DragDelta += new DragDeltaEventHandler(HandleBottomRight);
             topLeft.DragDelta += new DragDeltaEventHandler(HandleTopLeft);
             topRight.DragDelta += new DragDeltaEventHandler(HandleTopRight);
         }
-
-        // Handler for resizing from the bottom-right.
+        
         void HandleBottomRight(object sender, DragDeltaEventArgs args)
         {
             FrameworkElement adornedElement = this.AdornedElement as FrameworkElement;
@@ -49,20 +45,16 @@ namespace Drawing_Board
 
             if (adornedElement == null || hitThumb == null) return;
             FrameworkElement parentElement = adornedElement.Parent as FrameworkElement;
-
-            // Ensure that the Width and Height are properly initialized after the resize.
+            
             EnforceSize(adornedElement);
-
-            // Change the size by the amount the user drags the mouse, as long as it's larger 
-            // than the width or height of an adorner, respectively.
+            
             adornedElement.Width = Math.Max(adornedElement.Width + args.HorizontalChange, hitThumb.DesiredSize.Width);
             adornedElement.Height = Math.Max(args.VerticalChange + adornedElement.Height, hitThumb.DesiredSize.Height);
 
             visualChildren.Remove(dottedLine);
             isExist = true;
         }
-
-        // Handler for resizing from the top-right.
+        
         void HandleTopRight(object sender, DragDeltaEventArgs args)
         {
             FrameworkElement adornedElement = this.AdornedElement as FrameworkElement;
@@ -70,14 +62,10 @@ namespace Drawing_Board
 
             if (adornedElement == null || hitThumb == null) return;
             FrameworkElement parentElement = adornedElement.Parent as FrameworkElement;
-
-            // Ensure that the Width and Height are properly initialized after the resize.
+            
             EnforceSize(adornedElement);
-
-            // Change the size by the amount the user drags the mouse, as long as it's larger 
-            // than the width or height of an adorner, respectively.
+            
             adornedElement.Width = Math.Max(adornedElement.Width + args.HorizontalChange, hitThumb.DesiredSize.Width);
-            //adornedElement.Height = Math.Max(adornedElement.Height - args.VerticalChange, hitThumb.DesiredSize.Height);
 
             double height_old = adornedElement.Height;
             double height_new = Math.Max(Math.Abs(adornedElement.Height - args.VerticalChange), hitThumb.DesiredSize.Height);
@@ -88,22 +76,15 @@ namespace Drawing_Board
             visualChildren.Remove(dottedLine);
             isExist = true;
         }
-
-        // Handler for resizing from the top-left.
+        
         void HandleTopLeft(object sender, DragDeltaEventArgs args)
         {
             FrameworkElement adornedElement = AdornedElement as FrameworkElement;
             Thumb hitThumb = sender as Thumb;
 
             if (adornedElement == null || hitThumb == null) return;
-
-            // Ensure that the Width and Height are properly initialized after the resize.
+            
             EnforceSize(adornedElement);
-
-            // Change the size by the amount the user drags the mouse, as long as it's larger 
-            // than the width or height of an adorner, respectively.
-            //adornedElement.Width = Math.Max(adornedElement.Width - args.HorizontalChange, hitThumb.DesiredSize.Width);
-            //adornedElement.Height = Math.Max(adornedElement.Height - args.VerticalChange, hitThumb.DesiredSize.Height);
 
             double width_old = adornedElement.Width;
             double width_new = Math.Max(Math.Abs(adornedElement.Width - args.HorizontalChange), hitThumb.DesiredSize.Width);
@@ -120,21 +101,16 @@ namespace Drawing_Board
             visualChildren.Remove(dottedLine);
             isExist = true;
         }
-
-        // Handler for resizing from the bottom-left.
+        
         void HandleBottomLeft(object sender, DragDeltaEventArgs args)
         {
             FrameworkElement adornedElement = AdornedElement as FrameworkElement;
             Thumb hitThumb = sender as Thumb;
 
             if (adornedElement == null || hitThumb == null) return;
-
-            // Ensure that the Width and Height are properly initialized after the resize.
+            
             EnforceSize(adornedElement);
-
-            // Change the size by the amount the user drags the mouse, as long as it's larger 
-            // than the width or height of an adorner, respectively.
-            //adornedElement.Width = Math.Max(adornedElement.Width - args.HorizontalChange, hitThumb.DesiredSize.Width);
+            
             adornedElement.Height = Math.Max(args.VerticalChange + adornedElement.Height, hitThumb.DesiredSize.Height);
 
             double width_old = adornedElement.Width;
@@ -146,17 +122,14 @@ namespace Drawing_Board
             visualChildren.Remove(dottedLine);
             isExist = true;
         }
-
-        // Arrange the Adorners.
+        
         protected override Size ArrangeOverride(Size finalSize)
         {
             Point point = this.TransformToAncestor(this).Transform(new Point(0, 0));
-
-            // desiredWidth and desiredHeight are the width and height of the element that's being adorned.  
-            // These will be used to place the ResizingAdorner at the corners of the adorned element.  
+  
             double desiredWidth = AdornedElement.DesiredSize.Width;
             double desiredHeight = AdornedElement.DesiredSize.Height;
-            // adornerWidth & adornerHeight are used for placement as well.
+
             double adornerWidth = this.DesiredSize.Width;
             double adornerHeight = this.DesiredSize.Height;
 
@@ -175,20 +148,16 @@ namespace Drawing_Board
 
                 visualChildren.Add(dottedLine);
             }
-
-            // Return the final size.
+            
             return finalSize;
         }
-
-        // Helper method to instantiate the corner Thumbs, set the Cursor property, 
-        // set some appearance properties, and add the elements to the visual tree.
+        
         void BuildAdornerCorner(ref Thumb cornerThumb, Cursor customizedCursor)
         {
             if (cornerThumb != null) return;
 
             cornerThumb = new Thumb();
-
-            // Set some arbitrary visual characteristics.
+            
             cornerThumb.Cursor = customizedCursor;
             cornerThumb.Height = cornerThumb.Width = 10;
             cornerThumb.Opacity = 1;
@@ -197,10 +166,7 @@ namespace Drawing_Board
 
             visualChildren.Add(cornerThumb);
         }
-
-        // This method ensures that the Widths and Heights are initialized.  Sizing to content produces
-        // Width and Height values of Double.NaN.  Because this Adorner explicitly resizes, the Width and Height
-        // need to be set first.  It also sets the maximum size of the adorned element.
+        
         void EnforceSize(FrameworkElement adornedElement)
         {
             if (adornedElement.Width.Equals(Double.NaN))
@@ -215,8 +181,7 @@ namespace Drawing_Board
                 adornedElement.MaxWidth = parent.ActualWidth;
             }
         }
-        // Override the VisualChildrenCount and GetVisualChild properties to interface with 
-        // the adorner's visual collection.
+
         protected override int VisualChildrenCount { get { return visualChildren.Count; } }
         protected override Visual GetVisualChild(int index) { return visualChildren[index]; }
     }
